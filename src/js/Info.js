5f306e4bdb73da54933b7770e8b7eb6b441267e6
Jim Binder <Jim1720@aol.com>
2022-01-07 11:10:03 -0800

    fix instructions

--- src/js/Info.js
+++ src/js/Info.js
@@ -23,21 +23,30 @@ class Info extends React.Component {
           
             <div> 
           
-            <p className="burleywood"> Use this app to register as a customer then enter and adjust claims.</p> 
+            <p className="burleywood"> Use this app to register as a customer then enter, adjust and pay claims.</p> 
   
           <ul>
-            <li> register - Enter customer info : you will be prompted to select a customer
-                identification number and password. A promotion code will be provided to
-                allow registeration. 
+            <li> Register - Enter customer info. You will be prompted to select a customer
+                identification number and password.  
             </li>
-            <li> update - Revise customer information. </li>
-            <li> plan - Assign a plan before claims can be filed. </li>
+            <li> Update - Allows updates to customer information. </li>
+            <li> Plan - Assign a plan before claims can be filed. </li>
             
-            <li> claim - Enter and submit a claim. You must select a claim type: medical, dental etc. </li>
+            <li> Claim - Enter and submit a claim. You must select a claim type: medical, dental etc. </li>
                     
             
-            <li> history - Review claim history. An option is provided to adjust a claim with a new one,
-                or pay an existing claim. 
+            <li> History - Review claim history. An option is provided to adjust a claim with a new one,
+                or pay an existing claim. A row of actions appear after each claim. You can use:
+                claim (new claim), Adjust or Pay the claim. Stay on returns you immediately to the claim
+                screen. Focus on positions at the operative claim.
+            </li>
+            <li>
+              Style - the update and claim screens have a style option that allows you to
+              adjust the look of the screen. The color link allows you to change colors.
+            </li>
+
+            <li>
+              An administrative option exists for site admins and is restricted.
             </li>
             </ul>
           
